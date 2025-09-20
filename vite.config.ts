ad1499822785f1e64dbf90a56086f1fb58ab352e
Fehmi Baltaci <baltacifehmi@gmail.com>
2025-09-20 16:56:48 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Use absolute paths for GitHub Pages
-  build: {
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    },
-  },
-})
-
+  base: "/fbaltaci-portfolio/",
+});
